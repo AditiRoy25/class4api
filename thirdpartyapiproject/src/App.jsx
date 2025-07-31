5731dafc294649fe1a376986f6227ca122911113
AditiRoy25 <aditiroyinfo.2016@gmail.com>
2025-07-31 12:39:06 +0530

    changes

--- thirdpartyapiproject/src/App.jsx
+++ thirdpartyapiproject/src/App.jsx
@@ -8,13 +8,6 @@ function App() {
     <>
       <NewsApi/>
 
-
-{/* https://api.github.com/users/${username}   */}
-{/* 
-https://ipapi.com/?utm_source=FirstPromoter&utm_medium=Affiliate&fpr=sajid13&msclkid=ead523491bb11e8f7af64bc3ea3884d2  - */}
-
-
-
     </>
   )
 }
